76574f6669d896d2440a0153d4e56ab185386585
NM1 <canilmmc@gmail.com>
2024-09-16 21:27:50 -0300

    1.0.0

---
+++ Patches/PlayerControllerB_Patch.cs
@@ -0,0 +1,22 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ArFe_ChaosMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerB_Patch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void Update_PostUpdate(ref bool ___isSprinting, ref float ___sprintMultiplier)
+        {
+            if (___isSprinting) 
+            {
+                ___sprintMultiplier = Mathf.Lerp(___sprintMultiplier,
+                                            6.75f,
+                                            Time.deltaTime * 30f);
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using ArFe_ChaosMod.Patches;
+using Random = UnityEngine.Random;
+
+namespace ArFe_ChaosMod
+{
+    [BepInPlugin(modGUID, modNAME, modVER)]
+    public class Main : BaseUnityPlugin
+    {
+        // MOD Info
+        public const string modGUID = "ArrFel.3xSpeedMod";
+        public const string modNAME = "Arroz's 3x Speed Mod";
+        public const string modVER = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        // Objects / References
+        internal ManualLogSource mls;
+        public static Main inst;
+
+        void Awake() 
+        {
+            if (inst == null) { inst = this; }
+            else if (inst != this) { Destroy(this); }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("3X Speed Mod has Awoken");
+
+            mls.LogInfo("Patching Scripts...");
+            harmony.PatchAll(typeof(Main));
+            harmony.PatchAll(typeof(PlayerControllerB_Patch));
+            mls.LogInfo("Patched Scripts.");
+        }
+
+
+    }
+}
